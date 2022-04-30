049257baa9d8decbb4b8c9e274319862bd71a0cd
PickleRick <bmkersey@gmail.com>
2022-04-30 00:53:45 -0400

    added timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
 var codeBox = document.querySelector("#code-box")
 var startButton = document.querySelector("#start-quiz")
 var mainPage = document.querySelector("#main")
+var selectedAnswer = document.querySelector("#answer-list")
+var timer = document.querySelector("#timer")
 
-questionNumber = 0
+var questionNumber = 0
+var timeRemaining = 60
 
 var questionArr = ["Commonly used data types DO NOT include:", "The condition in an 'If / then statement' is enclosed with_____", "Arrays in Javascript can be used to store:", "String values must be enclosed within _____ when being assigned to variables.", "A very useful tool to use during development and debugging for printing content to the debugger is:"]
 
@@ -22,23 +25,38 @@ var deleteContent = function(){
     codeBox.remove();
 };
 
+var checkAnswer = function(){
+    if (!selectedAnswer){
+        var answerStatus = document.createElement("h2");
+        answerStatus.className = "answer-status";
+        answerStatus.className = "Incorrect!!"
+
+    }
+}
+
 
 var callQuestion = function() {
+
+   
+
     var questionBox = document.createElement("div");
     var questionEl = document.createElement("h2");
     questionEl.textContent = questionArr[0];
     var answerEL = document.createElement("ul");
+    answerEL.className = "answer-list"
     mainPage.appendChild(questionBox);
     questionBox.appendChild(questionEl);
     questionBox.appendChild(answerEL);
 
     for (var i = 0; i < 4; i++){
         var answerButtonEl = document.createElement("li");
-        answerButtonEl.textContent = answerArr[i][i];
+        answerButtonEl.textContent = answerArr[questionNumber][i];
         answerButtonEl.className = "answer" + i
-        questionBox.appendChild(answerButtonEl)
+        answerEL.appendChild(answerButtonEl)
 
     }
+
+    checkAnswer()
 
 
 };
@@ -46,12 +64,29 @@ var callQuestion = function() {
 
 var startQuiz = function(){
 debugger;
+    
     if (questionNumber === 0){
         deleteContent();
     }
-    callQuestion();
 
     
+
+    callQuestion();
+
+    setInterval(quizTimer, 1000);
+    
+    function quizTimer(){
+        timeRemaining = timeRemaining - 1
+
+        if (timeRemaining <=0){
+            clearInterval(timeRemaining);
+            //counter ended add something here
+            return;
+        }
+
+        timer.innerHTML = "time: " + (timeRemaining)
+        
+    }
 
   
     
